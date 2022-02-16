618485900920f4a98db841d8352a6bc0c988c8f6
ManowYch <ychepaykin@gmail.com>
2022-02-16 17:02:06 +0100

    Add project files.

---
+++ ArrayYouTubeHomework/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace ArrayYouTubeHomework
+{
+    class Program
+    {
+        static int IndexChecker(int [] array,int element)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            int [5] MyArray = new Array;
+            Console.WriteLine("Введите искомый элемент");
+        }
+    }
+}
