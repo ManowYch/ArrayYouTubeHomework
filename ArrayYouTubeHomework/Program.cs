2f1ed0e78d54aeecf33610694820402276cbac92
ManowYch <ychepaykin@gmail.com>
2022-02-19 14:45:18 +0100

    First solution

--- ArrayYouTubeHomework/Program.cs
+++ ArrayYouTubeHomework/Program.cs
@@ -5,14 +5,39 @@ namespace ArrayYouTubeHomework
 {
     class Program
     {
-        static int IndexChecker(int [] array,int element)
+        private static int IndexChecker(int [] array, int element)
         {
-
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (element == array[i])
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
         static void Main(string[] args)
         {
-            int [5] MyArray = new Array;
+            int [] MyArray = new int[] { 1, 2, 4234, 5345 };
             Console.WriteLine("Введите искомый элемент");
+            bool isInt = Int32.TryParse(Console.ReadLine(),out int userElement);
+            if (isInt)
+            {
+               int result = IndexChecker(MyArray, userElement);
+                if (result != -1)
+                {
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("Совпадений не найдено");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка");
+            }
+            Console.ReadLine();
         }
     }
 }
